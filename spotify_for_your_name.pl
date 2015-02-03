302d0931355223e8ac54581145051ceacfef5d02
Jacqueline Abreu <jac@jacquelineabreu.me>
2015-02-03 09:02:11 -0200

    iniciando a aplicação que fará a playlist começando com músicas que começam com as letras do nome da pessoa logada. Utilizando mojolicius

---
+++ spotify_for_your_name.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Mojolicious::Lite;
+use JSON;
+use Text::Unaccent;
+use HTML::Entities;
+use Encode;
+
+get '/' => 'index';
+
+get '/login' => 'login';
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Example of the Authorization Code flow with Spotify</title>
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+            <style type="text/css">
+                #login, #loggedin {
+                display: none;
+                }
+                .text-overflow {
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    width: 500px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div id="login">
+                    <h1>This is an example of the Authorization Code flow</h1>
+                    <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+                    </div>
+                    <div id="loggedin">
+                    <div id="user-profile">
+                    </div>
+                    <div id="oauth">
+                    </div>
+                        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+                </div>
+            </div>
+        <body>
+    </html>
+
+@@login.html.ep
+<!doctype html>
+    <html>
+        <head>
+            <title>testando</title>
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+            <style type="text/css">
+                #login, #loggedin {
+                display: none;
+                }
+                .text-overflow {
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    width: 500px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div id="login">
+                    <h1>Login</h1>
+                </div>
+            </div>
+        </body>
+    </html>
